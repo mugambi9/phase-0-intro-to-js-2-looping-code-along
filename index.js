ade9e47975c8361cbfb7e8ea794f848af467d978
mugambi484 <123549183+mugambi484@users.noreply.github.com>
2024-01-08 11:28:15 +0300

    completed assignmnet

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// Code your solutions in this file
+
+
+
+function writeCards(names, eventName) {
+    
+    return names.map(name => `Thank you, ${name}, for the wonderful ${eventName} gift!`);
+  }
+  
+  function countDown(startingNumber) {
+    for (let i = startingNumber; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+ 
+
+  
